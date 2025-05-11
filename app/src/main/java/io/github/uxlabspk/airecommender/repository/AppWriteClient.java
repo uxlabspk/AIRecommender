bd3dcbba000fa9b7ad24501c16c293a7ec762223
Muhammad Naveed <muhammadnaveedcis@gmail.com>
2025-05-11 20:52:09 +0500

    Naveed : Updating AppWrite Client

--- app/src/main/java/io/github/uxlabspk/airecommender/repository/AppWriteClient.java
+++ app/src/main/java/io/github/uxlabspk/airecommender/repository/AppWriteClient.java
@@ -1,6 +1,23 @@
 package io.github.uxlabspk.airecommender.repository;
 
 
+import android.content.Context;
+
+import io.appwrite.Client;
+import io.appwrite.services.Storage;
+
 public class AppWriteClient {
+
+    public AppWriteClient() {
+
+    }
+
+    public Storage initializeStorage(Context context) {
+        Client client = new Client(context)
+                .setEndpoint("https://fra.cloud.appwrite.io/v1")
+                .setProject("6820c311002bbdf5ffcd");
+
+        return new Storage(client);
+    }
 
 }
